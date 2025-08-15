961b4c8d770cc8ae831a5959a7dd9b9ab317fa66
the_codex_ <138112742+codingguru2221@users.noreply.github.com>
2025-08-15 22:25:54 +0530

    hi

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Laddu = new Scanner(System.in);
+//        String Name = Laddu.nextLine(); // this will take input in String
+//        int a = Laddu.nextInt(); // this will take input as intiger
+
+        // mainly 5 variables are used in java programming language
+
+        int number = 5;
+        char ch = 'a';
+        float fl  = 2.14f;
+        double db = 2.34535d;
+        boolean bl = true;
+
+//        operation with string
+
+//        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//        System.out.println(txt.chars());
+
+        String intro = "hello my name is \"veerendra\"";
+        System.out.println(intro);
+
+    }
+}
